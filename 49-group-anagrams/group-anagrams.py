ba75c1cb200c06afb560744150cf0dd15ed06f1c
vinay-s-mathapati <vinaysm017@gmail.com>
2026-07-08 21:43:37 +0530

    Time: 12 ms (56.74%) | Memory: 21.9 MB (87.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,7 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = defaultdict(list)
-
         for word in strs:
             key = ''.join(sorted(word))
             groups[key].append(word)
