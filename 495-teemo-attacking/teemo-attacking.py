cffd40ea9db0b96105b069f27011b2d889afe92d
vinay-s-mathapati <vinaysm017@gmail.com>
2026-07-14 12:22:05 +0530

    Time: 19 ms (27.22%) | Memory: 20.6 MB (91.31%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.py
+++ 495-teemo-attacking/teemo-attacking.py
@@ -3,10 +3,8 @@ class Solution:
         if not timeSeries:
             return 0
         total = 0
-        t = len(timeSeries)
-        for i in range(t-1):
+        for i in range(len(timeSeries)-1):
             gap = timeSeries[i+1] - timeSeries[i]
-            
             total += min(duration, gap)
         total += duration
         return total
