c9a341522165d35d0c39785aa704f182495b1adc
vinay-s-mathapati <vinaysm017@gmail.com>
2026-07-17 11:59:09 +0530

    Time: 11 ms (47.95%) | Memory: 19.7 MB (26.51%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,6 +1,7 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         index_map = {}
+        
         for i in range(len(list1)):
             index_map[list1[i]] = i
         min_sum = float('inf')
