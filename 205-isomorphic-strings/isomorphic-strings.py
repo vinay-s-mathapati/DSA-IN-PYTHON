3bfc928a1a7984d95358a080224f5e5bac8be0f4
vinay-s-mathapati <vinaysm017@gmail.com>
2026-07-28 14:55:25 +0530

    Time: 3 ms (91.11%) | Memory: 19.1 MB (81.98%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -5,6 +5,7 @@ class Solution:
         for i in range(len(s)):
             ch1 = s[i]
             ch2 = t[i]
+            
             if ch1 in map_st:
                 if map_st[ch1] != ch2:
                     return False
