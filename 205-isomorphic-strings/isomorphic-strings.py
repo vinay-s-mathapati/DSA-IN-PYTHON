ca765184d667c445985ba50c060592958fe3a8c8
vinay-s-mathapati <vinaysm017@gmail.com>
2026-07-28 14:54:58 +0530

    Time: 3 ms (91.11%) | Memory: 19.2 MB (46.13%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map_st = {}
+        map_ts = {}
+        for i in range(len(s)):
+            ch1 = s[i]
+            ch2 = t[i]
+
+            if ch1 in map_st:
+                if map_st[ch1] != ch2:
+                    return False
+            else:
+                map_st[ch1] = ch2
+            if ch2 in map_ts:
+                if map_ts[ch2] != ch1:
+                    return False
+            else:
+                map_ts[ch2] = ch1
+        return True
+            
