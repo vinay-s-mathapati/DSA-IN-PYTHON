73578f7fe597f576f73ae29407687ee9a961c2fd
vinay-s-mathapati <vinaysm017@gmail.com>
2026-08-07 10:22:57 +0530

    Time: 23 ms (45.97%) | Memory: 19.7 MB (13.14%) - LeetSync

--- 749-shortest-completing-word/shortest-completing-word.py
+++ 749-shortest-completing-word/shortest-completing-word.py
@@ -2,7 +2,6 @@ class Solution:
     def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
         req = Counter(ch.lower() for ch in licensePlate if ch.isalpha())
         result = None
-
         for word in words:
             wc = Counter(word)
             if all(wc[c] >= req[c] for c in req):
